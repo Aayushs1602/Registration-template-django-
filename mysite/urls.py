79f3f728316b8ce680610e723880f7d33847c90f
Aayush.s1602 <65233835+Aayushs1602@users.noreply.github.com>
2023-03-18 11:30:54 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,8 +19,7 @@ from register import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("main.urls")),
     path('register/', v.register, name="register"),
-    path('', include("django.contrib.auth.urls"))
+    path('', include("django.contrib.auth.urls")),
 
 ]
